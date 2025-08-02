72aebd75290f3f14d751674253caaf38d32230d0
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2025-08-02 12:31:01 +0200

    Initial Commit

---
+++ ecp-client/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ecp-client
+//
+//  Created by Stephan on 2025/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ecp-client/ecp_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecp_clientApp.swift
+//  ecp-client
+//
+//  Created by Stephan on 2025/08/02.
+//
+
+import SwiftUI
+
+@main
+struct ecp_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
